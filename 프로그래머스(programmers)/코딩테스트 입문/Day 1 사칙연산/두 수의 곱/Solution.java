aff2931a7d9325169c5640e988ddce4faea0dec5
Kim <lumierebuds@gmail.com>
2024-10-20 21:07:44 +0900

    add: Day 1 사칙연산

---
+++ 프로그래머스(programmers)/코딩테스트 입문/Day 1 사칙연산/두 수의 곱/Solution.java
@@ -0,0 +1,13 @@
+package 코딩테스트_입문;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        boolean check = (num1 >=0 && num1 <= 100) && (num2 >= 0 && num2 <= 100); 
+        int answer = 0;
+        if(check){
+            answer = num1 * num2; 
+        } 
+        
+        return answer;
+    }
+}
---
+++ 프로그래머스(programmers)/코딩테스트 입문/Day 1 사칙연산/두 수의 차/Solution.java
@@ -0,0 +1,9 @@
+package 코딩테스트_입문;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+}
